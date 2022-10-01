fecc31f1f0baf5c1bfef6bc183dbd392f8a4a7aa
Panda1422 <114794478+Panda1422@users.noreply.github.com>
2022-10-01 12:12:33 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,15 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Chào Cô Gái Tháng 11",
+    introTitle: 'Nguyễn Thị Lan Anh',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
+    btnIntro: '^^IuIu^^',
+    title: 'Chúc mừng sinh nhật bé nha 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
+    btnYes: 'Em cảm ơn anh yêu đẹp zai <33',
+    btnNo: 'Không thèm cảm ơn :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
